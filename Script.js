979366d59b786e1df0119b76f69fdc3085b5c721
Dinesh Kumar <dineshsudhakaran2706@gmail.com>
2026-04-15 21:29:55 +0530

    the blog contents are added on ui

--- Script.js
+++ Script.js
@@ -4,14 +4,14 @@ function signup() {
   var age   = document.getElementById("age").value;
  
   if (name == "" || email == "" || age == "") {
-    document.getElementById("error").textContent = "Please fill all fields!";
+    document.getElementById("error").textContent = "Please fill all fields to use the blog page!";
     return;
   }
  
   if (age < 13) {
-    document.getElementById("error").textContent = "You must be at least 13!";
+    document.getElementById("error").textContent = "You must be at least 14!";
     return;
   }
  
   window.location.href = "blog.html";
-}
+}                                                                                     
